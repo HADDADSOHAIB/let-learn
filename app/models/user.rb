09efca72c08bb64e4a38cd91df04acf754d0a167
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-05-04 12:39:02 +0000

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  validates :username, :fullname, :email, presence: true
+  validates :username, length: { maximum: 20 },
+                      uniqueness: { case_sensitive: false }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX, message: 'The email is not valid' }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -63,5 +63,4 @@ ActiveRecord::Schema.define(version: 2020_05_02_122303) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
