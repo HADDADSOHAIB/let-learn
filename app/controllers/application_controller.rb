feeedf9d7b7fa56746185f3c39cb1fbe15dfdebf
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-05-05 23:49:59 +0000

    add edit profile option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :username, :photo, :cover_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :username, :photo, :cover_image, :password, :password_confirmation, :current_password])
   end
 end
