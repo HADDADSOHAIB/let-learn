396df42bd31d3726f211a6292dfd534626904574
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-05-05 16:29:52 +0000

    add max length to text area

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :thought
+
+  validates :text, :user_id, :thought_id, presence: true
+  validates :text, length: { maximum: 50 }
 end
