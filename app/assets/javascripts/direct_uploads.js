09c44a9a8b131439540c60f7f9cb16bbd7cca525
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-05-05 18:55:32 +0000

    fix js issues

--- app/assets/javascripts/direct_uploads.js
+++ app/assets/javascripts/direct_uploads.js
@@ -1,4 +1,4 @@
-addEventListener("direct-upload:initialize", event => {
+addEventListener("direct-upload:initialize", function(event){
   const { target, detail } = event;
   const { id, file } = detail;
   target.insertAdjacentHTML("beforebegin", `
@@ -10,19 +10,19 @@ addEventListener("direct-upload:initialize", event => {
   target.previousElementSibling.querySelector(`.direct-upload__filename`).textContent = file.name;
 });
 
-addEventListener("direct-upload:start", event => {
+addEventListener("direct-upload:start", function(event){
   const { id } = event.detail;
   const element = document.getElementById(`direct-upload-${id}`);
   element.classList.remove("direct-upload--pending");
 });
 
-addEventListener("direct-upload:progress", event => {
+addEventListener("direct-upload:progress", function(event){
   const { id, progress } = event.detail;
   const progressElement = document.getElementById(`direct-upload-progress-${id}`);
   progressElement.style.width = `${progress}%`;
 });
 
-addEventListener("direct-upload:error", event => {
+addEventListener("direct-upload:error",function(event){
   event.preventDefault();
   const { id, error } = event.detail;
   const element = document.getElementById(`direct-upload-${id}`);
@@ -30,7 +30,7 @@ addEventListener("direct-upload:error", event => {
   element.setAttribute("title", error);
 });
 
-addEventListener("direct-upload:end", event => {
+addEventListener("direct-upload:end",function(event){
   const { id } = event.detail;
   const element = document.getElementById(`direct-upload-${id}`);
   element.classList.add("direct-upload--complete");
