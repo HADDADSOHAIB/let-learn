9741fcaf1a74169a5651d9922f25885c936e334d
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-05-05 16:23:25 +0000

    add comments work flow

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,24 @@ class CommentsController < ApplicationController
   before_action :set_thought, only: [:show]
 
   def show
-    
+    @comment = Comment.new
     respond_to do |format|
       format.js
+    end
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @thought = @comment.thought
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to root_path, notice: 'comment was successfully created.' }
+        format.json { render :show, status: :created, location: @comment }
+        format.js
+      else
+        format.html { redirect_to root_path, alert: @comment.errors.full_messages.join(". ") }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -13,4 +28,7 @@ class CommentsController < ApplicationController
     @thought = Thought.find(params[:id])
   end
 
+  def comment_params
+    params.require(:comment).permit(:text, :user_id, :thought_id)
+  end
 end
