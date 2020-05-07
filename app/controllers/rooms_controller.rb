f3aeb2b42bf0b50da5bda90589d29db56094a3a4
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-05-07 15:25:37 +0000

    fix room bug

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,6 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show]
+  before_action :secure_room, only: [:show]
 
   def index
     @rooms = current_user.rooms.includes(:users).order(last_message: :desc)
@@ -8,10 +9,19 @@ class RoomsController < ApplicationController
 
   def show
     @rooms = current_user.rooms.includes(:users).order(last_message: :desc)
+    if !@room
+      redirect_to rooms_path, alert: "Room does not exist or you are not a member"
+    end
   end
 
   private
   def set_room
-    @room = Room.includes(:messages).find(params[:id])
+    @room = Room.includes(:messages).find_by_id(params[:id])
+  end
+
+  def secure_room
+    if @room && !@room.users.include?(current_user)
+      @room = nil
+    end
   end
 end
